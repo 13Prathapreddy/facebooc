7c80176d9fe7a92f5884426a6ed60238a2359b87
root <root@ip-172-31-42-4.eu-north-1.compute.internal>
2026-01-04 11:55:44 +0000

    Add Terraform configuration for ECS and ECR infrastructure

---
+++ terraform/ecs.tf
@@ -0,0 +1,26 @@
+resource "aws_ecs_cluster" "this" {
+  name = "${var.app_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = var.app_name
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_exec.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = var.app_name
+      image = "${aws_ecr_repository.app.repository_url}:${var.image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 16000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
